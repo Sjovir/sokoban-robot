2618d4c5422310b9afc94ae6eca6b20e386581f7
sjovir <erik@hagebjerg.dk>
2019-09-13 11:39:36 +0200

    robot-test

---
+++ main.py
@@ -0,0 +1,44 @@
+import ev3dev.ev3 as ev3
+from sys import exit
+
+btn = ev3.Button()
+
+motor1 = ev3.LargeMotor('outC')
+motor2 = ev3.LargeMotor('outD')
+
+motor1.run_direct()
+motor2.run_direct()
+
+speed = 50
+runTime = 3000
+stopLength = 50
+
+colorSensor = ev3.ColorSensor('in1')
+colorSensor.mode = 'COL-COLOR'
+
+ultrasonicSensor = ev3.UltrasonicSensor('in2')
+ultrasonicSensor.mode = 'US-DIST-CM'
+
+assert ultrasonicSensor.connected, "UltrasonicSensor is not connected"
+assert colorSensor.connected, "ColorSensor is not connected"
+
+lightData = ultrasonicSensor.value()
+
+while not btn.any():
+    lightData = ultrasonicSensor.value()
+    colorData = colorSensor.value()
+
+    if not lightData < stopLength and colorData is not 6:
+        motor1.duty_cycle_sp = speed
+        motor2.duty_cycle_sp = speed
+        print('Distance: ' + str(lightData) + ' - Color: ' + str(colorData))
+    else:
+        motor1.duty_cycle_sp = 0
+        motor2.duty_cycle_sp = 0
+
+motor1.duty_cycle_sp = 0
+motor2.duty_cycle_sp = 0
+print('Exit')
+exit(0)
+
+
