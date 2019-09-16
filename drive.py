2e1033850e8e2b51a98884c13e2fd669389f52ea
Lasse Traberg-Larsen <lasse@traberg-larsen.dk>
2019-09-16 11:12:19 +0200

    drive.py
    DriveBehavior added

---
+++ drive.py
@@ -0,0 +1,47 @@
+import ev3dev.ev3 as ev3
+from threading import Thread
+from time import sleep
+
+class DriveBehavior:
+    behavior: "Drive"
+    colorSensor = None
+    navigator = None
+    colorleft = 0
+    colorRight = 0
+    colorTarget = 0
+    running = False
+    thread = None
+
+    # Constructor.
+    def __init__(self, navigator, colorSensor, colorLeft, colorRight, colorTarget):
+        self.colorSensor = colorSensor
+        self.navigator = navigator
+        self.colorLeft = colorLeft
+        self.colorRight = colorRight
+        self.colorTarget = colorTarget
+
+    # Start Method.
+    def turn_On(self):
+        self.running = True
+        thread = Thread(target = follow_Line)
+        thread.start()
+
+    # Stop Method.
+    def turn_Off(self):
+        self.running = False
+
+    # Follow Line.
+    def follow_Line(self):
+        while(self.running):
+            colorData =  colorSensor.value()
+            if(colorData = self.colorRight):
+                navigator.left()
+            elif(colorData = self.colorLeft):
+                navigator.right()
+            else:
+                navigator.forward()
+        navigator.stop()
+    
+
+
+obj = DriveBehavior()
